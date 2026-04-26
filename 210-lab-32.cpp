a4745ac8a98682bca602ad3b7715e1d28e832bf4
gillianrhett <gillianrhett@gmail.com>
2026-04-26 12:55:02 -0700

    started lab 33

--- 210-lab-32.cpp
+++ 210-lab-32.cpp
@@ -1,33 +1,47 @@
-// COMSC 210 | Lab 32 | Gillian Rhett
-// This program simulates a line of cars lined up to pay their toll at a toll booth. 
-// We will start with a certain number of cars in the line, and repeatedly perform these line operations until the line is empty:
-//    55% probability that the car at the head of the line pays its toll and leaves the toll booth
-//    45% probability that another car joins the line for the toll booth
+// COMSC 210 | Lab 33 | Gillian Rhett
+// This program simulates multiple toll lanes of cars lined up to pay their toll at a toll booth. 
+// for each lane:
+//    46% probability that the car at the head of the queue pays its toll and leaves the queue
+//    39% probability that another car joins the queue
+//    15% probability that the rear car will shift lanes
 
 #include <iostream>
 #include <cstdlib>
+#include <array>
 #include <deque>
 #include "Car.h"
 using namespace std;
 
 const int P_LEAVE = 55; // either a car leaves or a new car joins
 const int INIT_SIZE = 2; // initial size of the deque
+const int NUM_LANES = 4; // number of toll lanes to store in the array
+const int SIM_CYCLES = 20; // number of times to run the simulation
 
 int main() {
-    // create a deque to store the cars in the toll lane
-    deque <Car> toll_lane;
-    for(int i = 0; i < INIT_SIZE; ++i) {
-        Car new_car; // create a new car with random info
-        toll_lane.push_back(new_car); // add the new car to the back of the line
+    // create an array to store four lanes
+    array <deque <Car>, NUM_LANES> lanes;
+
+    // create 4 deques to store the cars in the toll lanes
+    for (int i = 0; i < NUM_LANES; ++i) {
+        deque <Car> temp_lane;
+        for(int j = 0; j < INIT_SIZE; ++j) {
+            Car new_car; // create a new car with random info
+            temp_lane.push_back(new_car); // add the new car to the back of the line
+        }
+        lanes.at(i) = temp_lane;
     }
 
     // display the cars in the lane at the beginning of the simulation
     cout << "Initial queue:" << endl;
-    for (Car c : toll_lane) {
-        cout << "\t";
-        c.print();
+    int n = 1;
+    for (deque toll_lane : lanes) {
+        cout << "Lane " << n++ << endl;
+        for (Car c : toll_lane) {
+            cout << "\t";
+            c.print();
+        }
     }
-
+/* // commenting out the rest so I can test the first part
     // simulate cycles of the toll lane until it is empty
     int count = 1;
     int prob;
@@ -58,5 +72,6 @@ int main() {
             cout << "\tEmpty"<< endl;
         cout << endl;
     }
+*/
     return 0;
 }
